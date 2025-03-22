a9af93bc37deb8bd071b0e16c75b818152fec85c
cyrus <cyrusgaudry@yahoo.fr>
2025-03-23 00:24:27 +0100

    555

--- app/mytestproject/mytestapp/models.py
+++ app/mytestproject/mytestapp/models.py
@@ -3,3 +3,7 @@ from django.db import models
 class Choice1(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+class anotherChoice1(models.Model):
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
